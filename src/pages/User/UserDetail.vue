265c87a517c3a600ce38dfe0251ccf720cff38e6
elliot richardson <ear78@aol.com>
2018-04-20 23:00:22 -0500

    added id to indicate which user is loaded in user details view

--- src/pages/User/UserDetail.vue
+++ src/pages/User/UserDetail.vue
@@ -1,9 +1,13 @@
 <template >
-    <h1>User Details</h1>
+    <div >
+        <h1>User Details</h1>
+        <p>User Loaded ID: {{ $route.params.id }}</p>
+    </div>
+
 </template>
 
 <script type="text/javascript">
     export default {
-        
+
     }
 </script>
