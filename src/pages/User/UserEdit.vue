7b80b1bbb08deeb25a1d68c0e04eff27cbd25e2b
elliot richardson <ear78@aol.com>
2018-04-23 22:47:24 -0500

    added hash scroll

--- src/pages/User/UserEdit.vue
+++ src/pages/User/UserEdit.vue
@@ -3,6 +3,11 @@
         <h1>User Edit</h1>
         <p>Locale: {{ $route.query.locale }}</p>
         <p>Analytics: {{ $route.query.q }}</p>
+        <a href="#data">Go to data</a>
+        <div style="height: 700px;background-color:lightgray;">
+
+        </div>
+        <p id="data">Some extra data</p>
     </div>
 
 </template>
